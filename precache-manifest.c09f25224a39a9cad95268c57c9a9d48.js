a1c4e0169a4c243fbeeaf76835e3abebcc657898
unknown <you@example.com>
2020-05-03 15:45:28 +0600

    Updates

---
+++ precache-manifest.c09f25224a39a9cad95268c57c9a9d48.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c028465756c296fff73def02a1dbce8",
+    "url": "/multifunctional-todo-app/index.html"
+  },
+  {
+    "revision": "3acc08f03f648f75de26",
+    "url": "/multifunctional-todo-app/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "878dcf39002be63209bb",
+    "url": "/multifunctional-todo-app/static/css/main.73380640.chunk.css"
+  },
+  {
+    "revision": "3acc08f03f648f75de26",
+    "url": "/multifunctional-todo-app/static/js/2.c24c225c.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/multifunctional-todo-app/static/js/2.c24c225c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "878dcf39002be63209bb",
+    "url": "/multifunctional-todo-app/static/js/main.a7cb3dbe.chunk.js"
+  },
+  {
+    "revision": "c9fd84d087fb3210c5db",
+    "url": "/multifunctional-todo-app/static/js/runtime-main.0aa23560.js"
+  }
+]);
